61d756c9bd0594f351c3c4ad4145e17aa97ba786
Fvkami <gamemer.p1.10@gmail.com>
2025-11-12 15:11:22 +0700

    edit

--- Health-Queuee/pages/ManageTheDocter.jsx
+++ Health-Queuee/pages/ManageTheDocter.jsx
@@ -2,26 +2,6 @@ import { useState } from "react";
 import { Button } from "react-bootstrap";
 
 const ManageTheDocter = () => {
-
-    //search Docter
-    function SearchDocter() {
-        const [SearchName, SetSearchName] = useState('')
-        const items = [
-            { id: 1, name: 'นายแพทย์	ธีรวัฒน์ วรธนันท์' },
-            { id: 2, name: 'แพทย์หญิง กานต์สินี ไพศาลสกุล' },
-            { id: 3, name: 'นายแพทย์	พงศกร รัตนโกสินทร์' },
-            { id: 4, name: 'แพทย์หญิง อารยา ศิริพัฒนากุล' }
-        ]
-
-        const handleSearchChange = (event) => {
-            SetSearchName(event.target.value)
-        }
-
-        const filteredItems = items.filter((item) =>
-            item.name.toLowerCase().includes(SearchName.toLowerCase())
-        )
-    }
-
     return (
         <div>
             <div>
