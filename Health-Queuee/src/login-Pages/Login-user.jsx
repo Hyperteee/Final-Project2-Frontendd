7650ef519426ad34f994ea1f9b944c599a267720
Dev Thee <theefordev@gmail.com>
2025-11-17 00:12:24 +0700

    Update Login-user.jsx

--- Health-Queuee/src/login-Pages/Login-user.jsx
+++ Health-Queuee/src/login-Pages/Login-user.jsx
@@ -2,17 +2,23 @@ export default function LoginPage() {
   return (
     <div className="container-fluid vh-100 p-0">
       <div className="row g-0 h-100">
-        <div className="col-md-6 text-white d-none d-md-flex flex-column justify-content-between p-5"
-  style={{
-    backgroundImage: "url('./images/login-bg.jpeg')",
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-    backgroundRepeat: "no-repeat",
-  }}>
+        <div
+          className="col-md-6 text-white d-none d-md-flex flex-column justify-content-between p-5"
+          style={{
+            backgroundImage: "url('./images/login-bg.jpeg')",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+          }}
+        >
           <div>
-            <h3 className="mb-5" style={{ marginTop: "-50px" }}>
-              <img src="./images/HFU-Logo.png" alt="HFU" className="h-48 w-auto " />
-            </h3>
+            {/* <h3 className="mb-5" style={{ marginTop: "-50px" }}>
+              <img
+                src="./images/HFU-Logo.png"
+                alt="HFU"
+                className="h-48 w-auto "
+              />
+            </h3> */}
           </div>
         </div>
 
@@ -25,14 +31,44 @@ export default function LoginPage() {
                 <label htmlFor="login" className="form-label fw-semibold">
                   Login
                 </label>
-                <input type="text" className="form-control" id="login" placeholder="ชื่อผู้ใช้งาน" />
+                <input
+                  type="text"
+                  className="form-control"
+                  id="login"
+                  placeholder="ชื่อผู้ใช้งาน"
+                />
               </div>
 
               <div className="mb-3">
                 <label htmlFor="password" className="form-label fw-semibold">
                   Password
                 </label>
-                <input type="password" className="form-control" id="password" placeholder="รหัสผ่าน" />
+                <input
+                  type="password"
+                  className="form-control"
+                  id="password"
+                  placeholder="รหัสผ่าน"
+                />
+
+                {/* sign in */}
+                <div
+                  className="d-grid gap-2"
+                  style={{
+                    marginTop: "20px",
+                  }}
+                >
+                  <button
+                    type="button"
+                    className=" border d-flex align-items-center justify-content-center gap-2 py-2"
+                    style={{
+                      backgroundColor: "#3b77fa",
+                      color: "#fff",
+                      borderRadius: "8px",
+                    }}
+                  >
+                    <span className="fw-semibold text-white">Sign In</span>
+                  </button>
+                </div>
               </div>
 
               <div className="mb-4">
@@ -47,13 +83,30 @@ export default function LoginPage() {
               <div className="mb-3">
                 <p className="text-center mb-3">หรือ เข้าสู่ระบบด้วย</p>
                 <div className="d-grid gap-2">
-                  <button type="button" className="btn btn-dark">
-                    <i className="bi bi-apple me-2"></i>
-                    เบอร์โทรศัพทย์
+                  <button
+                    type="button"
+                    className="btn btn-light border d-flex align-items-center justify-content-center gap-2 py-2"
+                    style={{ borderRadius: "8px" }}
+                  >
+                    <img
+                      src="./images/facebook-logo.png"
+                      alt="Facebook"
+                      style={{ width: "24px", height: "24px" }}
+                    />
+                    <span className="fw-semibold text-dark">หมายเลขประจำตัวผู้ป่วย</span>
                   </button>
-                  <button type="button" className="btn btn-dark">
-                    <i className="bi bi-google me-2"></i>
-                    Email
+
+                  <button
+                    type="button"
+                    className="btn btn-light border d-flex align-items-center justify-content-center gap-2 py-2"
+                    style={{ borderRadius: "8px" }}
+                  >
+                    <img
+                      src="./images/google-logo.png"
+                      alt="Google"
+                      style={{ width: "24px", height: "24px" }}
+                    />
+                    <span className="fw-semibold text-dark">เบอร์โทร</span>
                   </button>
                 </div>
               </div>
@@ -70,5 +123,5 @@ export default function LoginPage() {
         </div>
       </div>
     </div>
-  )
+  );
 }
